ef2c212d9b5cd6e1d81e981aa1a8eed20f2a0758
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2018-05-19 17:04:27 -0700

    Fix bankAccounts fetchData to return a Promise.all for synchronous upsert of bankAccounts and bankAccountTransactions

--- app/server/fetchData/bankAccounts.js
+++ app/server/fetchData/bankAccounts.js
@@ -1,19 +1,25 @@
 import axios from 'axios';
 
+const insertBankAccountsForUser = (response) => {
+  const {data: bankAccounts} = response;
+
+  bankAccounts.forEach((bankAccount) => {
+    const modeledBankAccount = new BankAccount(bankAccount);
+    const {id} = modeledBankAccount;
+    BankAccounts.upsert({id}, {$set: modeledBankAccount});
+  });
+};
+
 export default function fetchBankAccounts() {
   const baseUrl = 'http://nw-angelhack-2018-mocks.us-east-1.elasticbeanstalk.com';
   const customers = Customers.find().fetch(); // Note: never do this in a real project
 
+  const promises = [];
   customers.forEach((customer) => {
-    const { id } = customer;
-    axios.get(`${baseUrl}/customers/${id}/bankAccounts`).then((response) => {
-      const { data: bankAccounts } = response;
-
-      bankAccounts.forEach((bankAccount) => {
-        const modeledBankAccount = new BankAccount(bankAccount);
-        const { id } = modeledBankAccount;
-        BankAccounts.upsert({id}, {$set: modeledBankAccount});
-      });
-    })
+    const url = `${baseUrl}/customers/${customer.id}/bankAccounts`;
+    const promise = axios.get(url).then(insertBankAccountsForUser);
+    promises.push(promise);
   });
+
+  return Promise.all(promises);
 }
